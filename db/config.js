4c79d4623cee7c623a2f6d808803d04340b7f850
vlakiastoray96 <vlakiastoray96@gmail.com>
2026-04-18 15:22:40 -0500

    fix: se actualizo el codigo

--- db/config.js
+++ db/config.js
@@ -1,17 +1,22 @@
 const { config } = require('../config/config.js');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
+//const USER = encodeURIComponent(config.dbUser);
+//const PASSWORD = encodeURIComponent(config.dbPassword);
 // Importante: La propiedad debe llamarse 'url' para que Sequelize la reconozca automáticamente
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
-    url: URI,
-    dialect: 'postgres',
+    url: config.dbUrl,
+    dialect: config.dbEngine,
   },
   production: {
-    url: URI,
-    dialect: 'postgres',
+    url: config.dbUrl,
+    dialect: config.dbEngine,
+    dialectOptions: {
+      ssl : {
+        rejectUnauthorized: false
+      }
+    }
   }
 }
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,19 +1,24 @@
 const { Sequelize } = require('sequelize');
-
 const { config } = require('../config/config.js');
 const setupModels = require('../db/models')
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const options = {
+    dialect: config.dbEngine,
+    logging: config.isProd ? false : console.log
+};
 
-const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
-    logging: true,
-});
+if (config.isProd) {
+    options.dialectModule = require('pg');
+}
 
-setupModels(sequelize);
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-sequelize.sync();
+const sequelize = new Sequelize(config.dbUrl, options);
 
-module.exports = sequelize;
+// setupModels(sequelize);
+
+// sequelize.sync();
+
+// module.exports = sequelize;
